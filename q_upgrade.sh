9b11bd8ebaf0c294bb661c9df923d871de4e96b7
matija8 <matijanme@gmail.com>
2020-09-09 12:13:25 +0200

    Added q_upgrade.sh⬆️ script.

---
+++ q_upgrade.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+# Quick apt update & upgrade script.
+
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
+printf "\n\n${RED}Update:${NC}\n\n"
+sudo apt update
+
+printf "\n\n${RED}Upgrade:${NC}\n\n"
+sudo apt upgrade -y
+
+printf "\n\n${RED}Autoremove:${NC}\n\n"
+sudo apt autoremove
