be34b63f3f4c21f4c50b5d66d22a5cc2996e47f3
matija8 <matijanme@gmail.com>
2021-04-26 14:34:04 +0200

    Added link_to_apps and clear_port scripts.

---
+++ link_to_apps.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# coding: UTF-8
+
+# Change working directory to this scripts directory.
+cd $(dirname "$0")
+# echo $(pwd)
+
+files_to_link=(q_update.sh clear_port.sh dl_playlist.sh)
+
+# https://stackoverflow.com/questions/47367985/expanding-a-bash-array-only-gives-the-first-element
+for file in ${files_to_link[@]};
+do
+    echo "Updating $file"
+    ln -sf "$(pwd)/$file" ~/Apps/bin/$file
+done
+
+echo "Linking done!"
